5bf3c8409c740195b8674e9bceef2d8de3db8812
dustan1 <goadustan@gmail.com>
2014-03-23 17:37:23 -0400

    fasdfew

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "firstTen.h"
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    system("PAUSE");
-    return EXIT_SUCCESS;
+   objectFirst aso;
+   
+   cout<<aso.two(2,5)<<endl;
+   
+   
+   system("PAUSE");
+   return EXIT_SUCCESS;
 }
